0103c20978c43a7018c46d9eefbebc237511cfb2
gabrielburich <gabrielburich@gmail.com>
2021-01-10 15:40:51 -0300

    feat: project structure

---
+++ src/main/java/com/github/gabrielburich/invoicereports/InvoiceReportsApplication.java
@@ -0,0 +1,13 @@
+package com.github.gabrielburich.invoicereports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoiceReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoiceReportsApplication.class, args);
+    }
+
+}
