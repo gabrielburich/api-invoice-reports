ffb864a2aa06254436abc41b2a0e443e2a900929
gabrielburich <gabrielburich@gmail.com>
2021-01-10 15:56:36 -0300

    feat: add invoice domain

---
+++ src/main/java/com/github/gabrielburich/invoicereports/domain/Invoice.java
@@ -0,0 +1,24 @@
+package com.github.gabrielburich.invoicereports.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Invoice")
+public class Invoice {
+
+    @Id
+    private String id;
+    private User author;
+    private Customer customer;
+    private List<Item> items;
+    private String paymentCondition;
+    private String deadlineDescription;
+    private Long daysValidity;
+
+    private final LocalDate creationDate = LocalDate.now();
+}
